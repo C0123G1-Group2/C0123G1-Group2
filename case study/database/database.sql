1bccc98f978bbc9f02143db13b3385b386c4b813
hovien2201 <124018097+hovien2201@users.noreply.github.com>
2023-05-01 22:40:22 +0700

    Create database.sql

---
+++ case study/database/database.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE threelion_management;
+USE threelion_management;
+CREATE TABLE dich_vu(
+ma_dich_vu INT PRIMARY KEY,
+ten_dich_vu VARCHAR(20) NOT NULL,
+gia DOUBLE NOT NULL);
+CREATE TABLE loai_dich_vu(
+ma_loai_dich_vu INT PRIMARY KEY AUTO_INCREMENT,
+ma_dich_vu INT, FOREIGN KEY(ma_dich_vu) REFERENCES dich_vu(ma_dich_vu),
+ten_loai_dich_vu VARCHAR(20) NOT NULL UNIQUE
+ );
+ CREATE TABLE khach_hang(
+ ma_khach_hang INT PRIMARY KEY,
+ ten_khach_hang VARCHAR(20) NOT NULL,
+ so_dien_thoai INT NOT NULL UNIQUE,
+ dia_chi VARCHAR(50),
+ email VARCHAR(20) NOT NULL UNIQUE
+ );
+ CREATE TABLE hoa_don(
+ ma_hoa_don INT PRIMARY KEY,
+ ma_khach_hang INT, FOREIGN KEY(ma_khach_hang) REFERENCES khach_hang(ma_khach_hang),
+ ma_loai_dich_vu INT, FOREIGN KEY(ma_loai_dich_vu) REFERENCES loai_dich_vu(ma_loai_dich_vu),
+ gio_bat_dau DATETIME NOT NULL,
+ so_gio_dat INT
+ );
+ CREATE TABLE hoa_don_chi_tiet (
+ ma_hoa_don_chi_tiet INT PRIMARY KEY,
+ ma_hoa_don INT, FOREIGN KEY(ma_hoa_don) REFERENCES hoa_don(ma_hoa_don),
+ tong_tien INT
+ );
+INSERT INTO dich_vu VALUES(1,"Sân 5",400000),
+(2,"Sân 7",590000),
+(3,"Sân futsal",600000);
+INSERT INTO loai_dich_vu VALUES (1,1,"Sân số 1"),
+(2,1,"Sân số 2"),
+(4,1,"Sân số 4"),
+(5,2,"Sân số 5"),
+(6,2,"Sân số 6"),
+(7,3,"Sân số 7"),
+(8,3,"Sân số 8");
+SELECT * FROM dich_vu dv
+JOIN loai_dich_vu ldv ON ldv.ma_dich_vu = dv.ma_dich_vu;
