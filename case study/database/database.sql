45d389ecca9f4891876fd76c583d60bbb8aa567c
hovien2201 <124018097+hovien2201@users.noreply.github.com>
2023-05-04 16:27:49 +0700

    Update database.sql

--- case study/database/database.sql
+++ case study/database/database.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE threelion_management;
 USE threelion_management;
 CREATE TABLE dich_vu(
-ma_dich_vu INT PRIMARY KEY,
+ma_dich_vu INT PRIMARY KEY AUTO_INCREMENT,
 ten_dich_vu VARCHAR(20) NOT NULL,
 gia DOUBLE NOT NULL);
 CREATE TABLE loai_dich_vu(
@@ -28,15 +28,16 @@ ten_loai_dich_vu VARCHAR(20) NOT NULL UNIQUE
  ma_hoa_don INT, FOREIGN KEY(ma_hoa_don) REFERENCES hoa_don(ma_hoa_don),
  tong_tien INT
  );
-INSERT INTO dich_vu VALUES(1,"Sân 5",400000),
-(2,"Sân 7",590000),
-(3,"Sân futsal",600000);
-INSERT INTO loai_dich_vu VALUES (1,1,"Sân số 1"),
-(2,1,"Sân số 2"),
-(4,1,"Sân số 4"),
-(5,2,"Sân số 5"),
-(6,2,"Sân số 6"),
-(7,3,"Sân số 7"),
-(8,3,"Sân số 8");
+INSERT INTO dich_vu (ten_dich_vu,gia) VALUES("Sân 5",400000),
+("Sân 7",590000),
+("Sân futsal",600000);
+INSERT INTO loai_dich_vu (ma_dich_vu,ten_loai_dich_vu) VALUES (1,"Sân số 1"),
+(1,"Sân số 2"),
+(1,"Sân số 4"),
+(2,"Sân số 5"),
+(2,"Sân số 6"),
+(3,"Sân số 7"),
+(3,"Sân số 8");
 SELECT * FROM dich_vu dv
 JOIN loai_dich_vu ldv ON ldv.ma_dich_vu = dv.ma_dich_vu;
+DROP DATABASE threelion_management
